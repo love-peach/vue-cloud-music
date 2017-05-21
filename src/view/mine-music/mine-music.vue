11ada886851a3db4197ebb9574672cac513c66b9
张晋佩 <302784355@qq.com>
2017-05-21 16:48:40 +0800

    修改我的音乐页面

--- src/view/mine-music/mine-music.vue
+++ src/view/mine-music/mine-music.vue
@@ -1,19 +1,98 @@
 <template>
-    <div>
-        <div>我的音乐页面</div>
+    <div class="min-music-wrap">
+        <mu-list class="option-list">
+            <div v-for="(item, index) in optionList">
+                <mu-list-item class>
+                    <i :class="[item.icon, 'iconfont', 'list-icon' ]" slot="left"></i>
+                    <span class="list-title">{{item.title}}</span>
+                    <span class="list-amount">({{item.amount}})</span>
+                </mu-list-item>
+                <mu-divider v-if="index !== optionList.length - 1 " inset/>
+            </div>
+        </mu-list>
+        <div class="abc">dfdf</div>
     </div>
 </template>
-<style>
+<style lang="less">
+    @import "../../style/variable";
 
+    @grayColor: #e7e9e9;
+    @leftOffset: 90px;
+    .min-music-wrap {
+        .option-list {
+            padding: 0;
+            .show-left {
+                .px2rem(padding-left, @leftOffset)
+            }
+            .mu-item-left {
+                .px2rem(left, 26);
+            }
+            .list-icon {
+                .px2rem(font-size, 42);
+                color: @themeColors;
+                line-height: 1;
+            }
+        }
+        .mu-divider.inset {
+            .px2rem(margin-left, @leftOffset)
+        }
+        .abc {
+            background: @grayColor;
+        }
+    }
 </style>
-<script>
+<script type="text/javascript">
+    import SongListCollecton from './SongListCollection.vue';
+    import * as promiseAjax from '../../common/promise-ajax'
+
     export default{
         data(){
-            return{
-                msg:'hello vue'
+            return {
+                optionList: [
+                    {
+                        title: '本地音乐',
+                        icon: 'icon-music',
+                        amount: 0
+                    }, {
+                        title: '最近播放',
+                        icon: 'icon-zuijinbofang',
+                        amount: 100
+                    }, {
+                        title: '下载管理',
+                        icon: 'icon-icon_xiazai',
+                        amount: 0
+                    }, {
+                        title: '我的电台',
+                        icon: 'icon-diantaibaoshe',
+                        amount: 1
+                    }, {
+                        title: '我的收藏',
+                        icon: 'icon-wodeshoucang',
+                        amount: 1
+                    },
+                ]
             }
         },
-        components:{
-        }
+        components: {
+            SongListCollecton
+        },
+        mounted() {
+            promiseAjax.get('/ajax/get', null, {errorTip: false, successTip: '成功了！'}).then(res => {
+                console.log(res);
+            }).catch(error => {
+                console.log(error);
+            }).finally(() => {
+                console.log(123);
+            });
+            const postAjsx = promiseAjax.get('/ajax/post', null, {errorTip: false, successTip: '成功了！'});
+            postAjsx.then(res => {
+                console.log(res);
+            }).catch(error => {
+                console.log(error);
+            }).finally(() => {
+                console.log(123);
+            });
+            postAjsx.cancel();
+        },
     }
 </script>
