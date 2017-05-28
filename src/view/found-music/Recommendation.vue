c4c7c8fd35a8d4b950fbb6846e5d6d3dc9cd8406
张晋佩 <302784355@qq.com>
2017-05-28 15:09:56 +0800

    添加轮播图组件

--- src/view/found-music/Recommendation.vue
+++ src/view/found-music/Recommendation.vue
@@ -1,30 +1,68 @@
 <template>
-    <div>
-        <div style="height: 1000px">个性推荐</div>
+    <div class="recommendation-wrap">
+        <swiper :options="swiperOption" ref="mySwiper">
+            <swiper-slide class="slider" v-for="(banner, index) in banners" :key="index">
+                <img class="slider-img" :src="banner.pic + cropImgStr">
+            </swiper-slide>
+            <div class="swiper-pagination"  slot="pagination"></div>
+        </swiper>
+        <div>dfdf</div>
+
     </div>
 </template>
-<style>
-
+<style scoped lang="less">
+    .recommendation-wrap {
+        .slider {
+            .slider-img {
+                width: 100%;
+            }
+        }
+    }
 </style>
 <script type="text/javascript">
 
     import axios from 'axios'
     import API from '../../api/API'
+    import VueAwesomeSwiper from 'vue-awesome-swiper'
+    import { swiper, swiperSlide } from 'vue-awesome-swiper';
 
     export default{
         data(){
             return{
-                msg:'hello vue'
+                swiperOption: {
+                    notNextTick: true,
+                    autoplay: 3000,
+                    direction : 'horizontal',
+                    grabCursor : true,
+                    setWrapperSize :true,
+                    // autoHeight: true,
+                    pagination : '.swiper-pagination',
+                    paginationClickable :true,
+                    observeParents:true,
+                    debugger: true,
+                    onTransitionStart(swiper){
+                        console.log(swiper)
+                    },
+                },
+                cropImgStr: '',
+                banners: [],
             }
         },
         components:{
+            swiper,
+            swiperSlide
+        },
+        computed: {
+            swiper: function() {
+                return this.$refs.mySwiper.swiper
+            }
         },
         mounted() {
-            API.get('/api/search', {
-                keywords: '成都'
-            })
-            API.get('/api/song/detail', {
-                ids: '347230'
+            this.cropImgStr = `?param=${window.innerWidth}x10000`
+            // this.swiper.slideTo(3, 1000, false);
+            API.get('/api/banner').then((res) => {
+                console.log(res, '000');
+                this.banners = res.banners;
             })
         },
         methods: {
